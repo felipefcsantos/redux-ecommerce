a886b245b9c31e42ed3ae0b5baf95cc780d7b424
Felipe Santos <felipesantos_fcs98@hotmail.com>
2023-08-31 16:29:43 -0300

    Adicionando a tela de Anuncie e as funções de Editar e deletar

--- src/pages/Categoria/index.js
+++ src/pages/Categoria/index.js
@@ -1,12 +1,14 @@
 import Header from 'components/Header'
 import React from 'react'
 import { useSelector } from 'react-redux'
-import { useParams } from 'react-router-dom'
+import { useNavigate, useParams } from 'react-router-dom'
 import styles from './Categoria.module.scss'
 import Item from 'components/Item'
+import Button from 'components/Button'
 
 export default function Categoria() {
   const { nomeCategoria } = useParams()
+  const navigate = useNavigate()
 
   const { categoria, itens } = useSelector(state => {
     const regexp = new RegExp(state.busca, 'i')
@@ -21,7 +23,11 @@ export default function Categoria() {
         titulo={categoria.nome}
         descricao={categoria.descricao}
         imagem={categoria.header}
-      />
+      >
+        <Button onClick={() => navigate(`/anuncie/${nomeCategoria}`)}>
+          Quero anunciar
+        </Button>
+      </Header>
       <div className={styles.itens}>
         {itens?.map(item => (
           <Item key={item.id} {...item} />
